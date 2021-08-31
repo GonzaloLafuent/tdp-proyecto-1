05483b891d3d40ada9268c861e44ab067a4817d0
Gonzalo Lafuente <lafuentegonzalo1@gmail.com>
2021-08-30 21:28:26 -0300

    subo foto y nuevos cambios en la interfaz

--- src/gui/SimplePresentationScreen.java
+++ src/gui/SimplePresentationScreen.java
@@ -2,7 +2,10 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.Toolkit;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,6 +16,7 @@ import entities.Student;
 import javax.swing.JTabbedPane;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
 
@@ -29,17 +33,21 @@ public class SimplePresentationScreen extends JFrame {
 	private JTextField nombreTextField;
 	private JTextField mailTextField;
 	private JTextField gitTextField;
+	private JLabel fotoLabel;
+	private JLabel fechaLabel;
+	private JLabel labelHora;
+	private JLabel fecha;
+	private JLabel hora;
 
 	public SimplePresentationScreen(Student studentData) {
 		this.studentData = studentData;
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		
 		setTitle("TdP-DCIC-UNS 2021 :: Pantalla de presentacion");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(new Dimension(615, 265));
+		setSize(new Dimension(615, 285));
 		setResizable(false);
 		setContentPane(contentPane);
 		
@@ -49,8 +57,10 @@ public class SimplePresentationScreen extends JFrame {
 	}
 	
 	private void init() {
+		contentPane.setLayout(null);
 		// Tabbed Pane to student personal data
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(5, 5, 430, 226);
 		tabInformation = new JPanel();
 		tabInformation.setPreferredSize(new Dimension(425, 275));
 		tabbedPane.addTab("Informacion del alumno", null, tabInformation, "Muestra la información declarada por el alumno");
@@ -141,6 +151,38 @@ public class SimplePresentationScreen extends JFrame {
 					.addContainerGap(15, Short.MAX_VALUE))
 		);
 		tabInformation.setLayout(gl_tabInformation);
-		contentPane.add(tabbedPane, BorderLayout.WEST);
+		contentPane.add(tabbedPane);
+		
+		fotoLabel = new JLabel("");
+		fotoLabel.setBounds(445, 48, 141, 148);
+		contentPane.add(fotoLabel);
+		
+		Image img = new ImageIcon(this.getClass().getResource("/perfil.jpg")).getImage();
+		Image imgResize = img.getScaledInstance(fotoLabel.getWidth(), fotoLabel.getHeight(), img.SCALE_SMOOTH);
+		fotoLabel.setIcon(new ImageIcon(imgResize));
+		
+		Date fechaActual = new Date();
+		
+		fechaLabel = new JLabel("Esta ventana fue generada el ");
+		fechaLabel.setBounds(15, 231, 175, 14);
+		contentPane.add(fechaLabel);
+		
+		SimpleDateFormat fechaAct = new SimpleDateFormat("dd/MM/yyyy");
+		
+		fecha = new JLabel("");
+		fecha.setBounds(186, 231, 78, 14);
+		contentPane.add(fecha);
+		fecha.setText(fechaAct.format(fechaActual));
+		
+		labelHora = new JLabel("a las");
+		labelHora.setBounds(254, 231, 33, 14);
+		contentPane.add(labelHora);
+	
+		SimpleDateFormat horaAct = new SimpleDateFormat("hh:mm:ss");
+		
+		hora = new JLabel("");
+		hora.setBounds(284, 231, 72, 14);
+		contentPane.add(hora);
+		hora.setText(horaAct.format(fechaActual));
 	}
 }
